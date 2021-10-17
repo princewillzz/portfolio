e77a1214b212d1266744b8828177304e86df7d76
princewillzz <princewillz2013@gmail.com>
2021-10-17 16:44:17 +0530

    refactored the layout a little bit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { AppColors } from "./assets/AppColors";
 import ContactMe from "./components/ContactMe";
+import { ExperienceContainer } from "./components/experience/ExperienceContainer";
 import Navbar from "./components/Navbar";
 import { SkillsContainer } from "./components/skills/SkillsContainer";
 import TopAbout from "./components/TopAbout";
@@ -13,15 +14,28 @@ function App() {
 		>
 			<Navbar sectionBackgroundColor={AppColors.backgroundBlue} />
 			<TopAbout sectionBackgroundColor={AppColors.backgroundBlue} />
+			{/* <WaveBorder
+				upColor={AppColors.backgroundBlue}
+				bottomColor={AppColors.darkBlue}
+			/> */}
+			<SkillsContainer sectionBackgroundColor={AppColors.backgroundBlue} />
 			<WaveBorder
 				upColor={AppColors.backgroundBlue}
 				bottomColor={AppColors.darkBlue}
 			/>
-			<SkillsContainer sectionBackgroundColor={AppColors.darkBlue} />
+
+			<ExperienceContainer sectionBackgroundColor={AppColors.darkBlue} />
 			<WaveBorder
 				upColor={AppColors.darkBlue}
+				bottomColor={AppColors.backgroundBlack}
+			/>
+
+			<SkillsContainer sectionBackgroundColor={AppColors.backgroundBlack} />
+			<WaveBorder
+				upColor={AppColors.backgroundBlack}
 				bottomColor={AppColors.grey}
 			/>
+
 			<ContactMe sectionBackgroundColor={AppColors.grey} />
 		</div>
 	);
---
+++ src/components/experience/ExperienceContainer.js
@@ -0,0 +1,19 @@
+
+import React from 'react'
+
+export const ExperienceContainer = ({ sectionBackgroundColor }) => {
+    return (
+        <div
+            id="Experience"
+            style={{
+                backgroundColor: sectionBackgroundColor
+            }}
+        >
+
+            <div className="experience-container">
+                
+            </div>
+            
+        </div>
+    )
+}
