9788530faac9d697ee4537eb71da9256691e5ea9
Harsh Tiwari <princewillz2013@gmail.com>
2023-04-11 22:00:50 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -101,7 +101,7 @@ export default function Navbar({ sectionBackgroundColor }: any) {
 						
 						<a 
 							className={`nav-link resume`} 
-							href="https://drive.google.com/file/d/1-wVzqyJuimMSa2SHzoh-YUVIj45-UzE2/view" 
+							href="https://drive.google.com/file/d/1Ilj0MxaS1SRcMSpG-jIuQZICstq1nSbC/view" 
 							target="_blank" 
 							rel="noopener noreferrer"
 						>
