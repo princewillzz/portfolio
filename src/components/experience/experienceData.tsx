443a4f16884398bb9a67fbd71da624488124eb01
Harsh Tiwari <harshtiwariai@gmail.com>
2023-04-22 23:03:51 +0530

    added toplyne experience

--- src/components/experience/experienceData.tsx
+++ src/components/experience/experienceData.tsx
@@ -1,46 +1,59 @@
 export const experienceRawData = Object.freeze({
-    data: [
-        {
-            companyName: "Rocketium",
-            jobRole: "Software Engineer",
-            description: [
-                "It is a SaaS product for creative and design process automation",
-                "Built media assets delivery integration to Facebook and Google ad Accounts",
-                "Worked on enhancing and fixing defects for in production software",
-                "Managed the setup, development and deployment of a few lambda functions written in Nodejs and Python(FastAPI)",
-                "Setup precommit, testing framework and deployment pipeline for a repository in bitbucket",
-                "Tech: Typescript, ReactJS, Nodejs, MongoDB, AWS lambda(Serverless), Python and FastAPI"
-            ],
-            startDate: "Mar 2022",
-            endDate: "Present",
-            websiteLink: 'https://rocketium.com/'
-        },
-        {
-            companyName: "Eleva Infotech",
-            jobRole: "Backend Engineer",
-            description: [
-                "Worked on a POS software that manages day to day functionality of restaurants",
-                "Developed order management APIs and UI using Express, ReactJS, MongoDB, facilitating execution of orders on various levels (waiter, chef, manager, etc)",
-                "Built authentication flow using firebase for both Client and backend app",
-                "Implemented custom Logging mechanism with size and time based backup",
-                "Built aggregation pipeline, Rest APIs and UI to process and present analytical data regarding overall sales, each item sales, inventory, with other filters"
-            ],
-            startDate: "Aug 2021",
-            endDate: "Mar 2022",
-            websiteLink: "https://www.linkedin.com/company/eleva-infotech-private-limited/"
-        },
-        {
-            companyName: "Chuhiya Software",
-            jobRole: "Software Engineer",
-            description: [
-                "Developed backend for ERP software and designed databases for the projects",
-                "Implemented features like role-based and OAuth Authentication and Authorization, in-memory Cache, payment gateway integration (Razorpay)",
-                "Integrated Google Drive storage to let users upload and download their data",
-                "Used WebSocket with STOMP subprotocol to build real-time status feed",
-                "Deployed projects on AWS using Elastic beanstalk and RDS"
-            ],
-            startDate: "Aug 2020",
-            endDate: "Feb 2021"
-        }
-    ]
-})
+	data: [
+		{
+			companyName: "Toplyne",
+			jobRole: "Software Engineer Intern",
+			description: [
+				"Built features like manage auth for integration during reverse ETL and revenue leakage calculator, including setting up CI/CD and deployment pipeline for a central repository of serverless Lamda functions",
+				"Revamped UI, fixed defects with major enhancements, and owned the security fixes increasing security grade from F to A+, improving overall product quality",
+				"Tech: Reactjs, Typescript, Python, Django, NodeJS, Serverless",
+			],
+			startDate: "Jan 2023",
+			endDate: "Present",
+			websiteLink: "https://toplyne.io/",
+		},
+		{
+			companyName: "Rocketium",
+			jobRole: "Software Development Intern",
+			description: [
+				"It is a SaaS product for creative and design process automation",
+				"Built media assets delivery integration to Facebook and Google ad Accounts",
+				"Worked on enhancing and fixing defects for in production software",
+				"Managed the setup, development and deployment of a few lambda functions written in Nodejs and Python(FastAPI)",
+				"Setup precommit, testing framework and deployment pipeline for a repository in bitbucket",
+				"Tech: Typescript, ReactJS, Nodejs, MongoDB, AWS lambda(Serverless), Python and FastAPI",
+			],
+			startDate: "Mar 2022",
+			endDate: "Dec 2022",
+			websiteLink: "https://rocketium.com/",
+		},
+		{
+			companyName: "Eleva Infotech",
+			jobRole: "Backend Engineer Intern",
+			description: [
+				"Worked on a POS software that manages day to day functionality of restaurants",
+				"Developed order management APIs and UI using Express, ReactJS, MongoDB, facilitating execution of orders on various levels (waiter, chef, manager, etc)",
+				"Built authentication flow using firebase for both Client and backend app",
+				"Implemented custom Logging mechanism with size and time based backup",
+				"Built aggregation pipeline, Rest APIs and UI to process and present analytical data regarding overall sales, each item sales, inventory, with other filters",
+			],
+			startDate: "Aug 2021",
+			endDate: "Mar 2022",
+			websiteLink:
+				"https://www.linkedin.com/company/eleva-infotech-private-limited/",
+		},
+		{
+			companyName: "Chuhiya Software",
+			jobRole: "Software Engineer Intern",
+			description: [
+				"Developed backend for ERP software and designed databases for the projects",
+				"Implemented features like role-based and OAuth Authentication and Authorization, in-memory Cache, payment gateway integration (Razorpay)",
+				"Integrated Google Drive storage to let users upload and download their data",
+				"Used WebSocket with STOMP subprotocol to build real-time status feed",
+				"Deployed projects on AWS using Elastic beanstalk and RDS",
+			],
+			startDate: "Aug 2020",
+			endDate: "Feb 2021",
+		},
+	],
+});
